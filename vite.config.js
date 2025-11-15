ac66395ce9c3902e7f4a51a391d87897a1f7a45e
AdForcer <vdkuziakin@gmail.com>
2025-11-16 02:28:01 +0300

    deployment test x5

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/https://inna-tarada.github.io//',
+  base: '/',
   build: {
     outDir: 'docs',
     emptyOutDir: true
-  },
-  publicDir: 'public'
+  }
 })
